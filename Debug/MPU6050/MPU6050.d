edcfeabab9d93b7b758ced49ac7ce8e0ce447e3a
minh_t57\minht57 <minhtri1996vl@gmail.com>
2017-07-31 12:35:20 +0700

    First add project

---
+++ Debug/MPU6050/MPU6050.d
@@ -0,0 +1,37 @@
+# FIXED
+
+MPU6050/MPU6050.obj: ../MPU6050/MPU6050.c
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/math.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/_defs.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h
+MPU6050/MPU6050.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h
+MPU6050/MPU6050.obj: ../MPU6050/MPU6050.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/inc/tm4c123gh6pm.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/inc/hw_memmap.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/gpio.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/pin_map.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/i2c.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/sysctl.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/timer.h
+MPU6050/MPU6050.obj: D:/ti/TivaWare_C_Series/driverlib/interrupt.h
+
+../MPU6050/MPU6050.c: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/math.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/_defs.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h: 
+../MPU6050/MPU6050.h: 
+D:/ti/TivaWare_C_Series/inc/tm4c123gh6pm.h: 
+D:/ti/TivaWare_C_Series/inc/hw_memmap.h: 
+D:/ti/TivaWare_C_Series/driverlib/gpio.h: 
+D:/ti/TivaWare_C_Series/driverlib/pin_map.h: 
+D:/ti/TivaWare_C_Series/driverlib/i2c.h: 
+D:/ti/TivaWare_C_Series/driverlib/sysctl.h: 
+D:/ti/TivaWare_C_Series/driverlib/timer.h: 
+D:/ti/TivaWare_C_Series/driverlib/interrupt.h: 
---
+++ Debug/main.d
@@ -0,0 +1,35 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/string.h
+main.obj: D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_ints.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_memmap.h
+main.obj: D:/ti/TivaWare_C_Series/inc/hw_types.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/gpio.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/interrupt.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/pin_map.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/sysctl.h
+main.obj: D:/ti/TivaWare_C_Series/driverlib/uart.h
+main.obj: ../MPU6050/MPU6050.h
+
+../main.c: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdint.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdio.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/linkage.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdarg.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/string.h: 
+D:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.1.LTS/include/stdbool.h: 
+D:/ti/TivaWare_C_Series/inc/hw_ints.h: 
+D:/ti/TivaWare_C_Series/inc/hw_memmap.h: 
+D:/ti/TivaWare_C_Series/inc/hw_types.h: 
+D:/ti/TivaWare_C_Series/driverlib/gpio.h: 
+D:/ti/TivaWare_C_Series/driverlib/interrupt.h: 
+D:/ti/TivaWare_C_Series/driverlib/pin_map.h: 
+D:/ti/TivaWare_C_Series/driverlib/sysctl.h: 
+D:/ti/TivaWare_C_Series/driverlib/uart.h: 
+../MPU6050/MPU6050.h: 
